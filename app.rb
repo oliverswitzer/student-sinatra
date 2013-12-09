360d09ef21a1f33043a89d5f7ac9c4344095e141
Oliver Switzer <oliverswitzer@gmail.com>
2013-12-09 12:53:37 -0500

    add artist, hello views that print out random artists and numbers respectively

--- app.rb
+++ app.rb
@@ -6,5 +6,15 @@ module StudentSite
     get '/' do
       "hello world!"
     end
+
+    get '/hello-world' do
+      @random_numbers = (1..20).to_a.shuffle
+      erb :hello
+    end
+
+    get '/artists' do
+      @artists = ["Deadmau5", "Koko", "Frank Zappa"]
+      erb :artists
+    end
   end
 end
